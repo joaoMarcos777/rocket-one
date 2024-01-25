c93200e229ff9921b16b94961062f4c4ec49800b
João Marcos <joaomarcos@frwk.com.br>
2024-01-25 17:28:41 -0300

    fundamentos em typescript

---
+++ aulas/01-fundamentos-reactjs-typescript/src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+import { Avatar } from "./Avatar";
+import { useState } from "react";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar hasBorder={false} src="https://github.com/joaoMarcos777.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João Marcos</strong>
+              <time title="11 de May às 8:13h" dateTime="2023-05-11 00:13:30">
+                About 1 hour ago
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Delete comment">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Applause
+            <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
